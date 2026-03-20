78475ebd2083a253b871aef641dce0a093d8809e
vanche93 <vanche93@yandex.ru>
2026-03-20 13:54:08 +0300

    Добавил проверку прошлого статуса тренировки перед отправкой уведомления

--- app/trainy/signals.py
+++ app/trainy/signals.py
@@ -1,12 +1,22 @@
-from django.db.models.signals import m2m_changed, post_save
+from django.db.models.signals import m2m_changed, pre_save
 from django.dispatch import receiver
 from .models import Training
 from .tg import telegram
 
+@receiver(pre_save, sender=Training)
+def check_training_open(sender, instance, **kwargs):
+    old_status = Training.objects.get(pk=instance.pk).status if instance.pk else None
+    print(old_status, instance.status)
+    if (
+        old_status
+        and instance.status == "open"
+        and old_status != "open"
+    ):
+        telegram.send_open_message(instance)
+
 @receiver(m2m_changed, sender=Training.topics.through)
 @receiver(m2m_changed, sender=Training.training_times.through)
-@receiver(post_save, sender=Training)
-def check_training_open(sender, instance, **kwargs):
+def check_training_open_m2m(sender, instance, **kwargs):
     if (
         instance.status == "open"
         and instance.topics.exists()
